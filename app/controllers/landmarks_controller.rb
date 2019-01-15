bdd4bbf50b16f4771f1f62fe3039a5ed2f963d54
Joshua Tan <jtsarvida@gmail.com>
2019-01-15 08:03:15 +0000

    Automatically backed up by Learn

--- app/controllers/landmarks_controller.rb
+++ app/controllers/landmarks_controller.rb
@@ -6,6 +6,7 @@ class LandmarksController < ApplicationController
   end
   
   get '/landmarks' do
+    @figures = Figure.all
     erb :'/figures/index'
   get '/figures' do
     @figures = Figure.all 
